3890ff21da55e72e95016fafc3d24cb2dd94efef
Torionj <t33zus@gmail.com>
2018-02-15 20:59:17 -0600

    update

---
+++ output.js
@@ -0,0 +1,12 @@
+function consolePrint(){
+    console.log("hello Torion");
+}
+function alertPrint(){
+        alert("hello Torion")
+}
+function documentPrint(){
+    document.write("hello Torion")
+}
+function goodBye(){
+    alert("GoodBye Jon")
+}
