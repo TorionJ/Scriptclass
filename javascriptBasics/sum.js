d2d9b9cd46d2b8c755dece2411ccd41f88ad5b9b
Torionj <t33zus@gmail.com>
2018-02-25 12:52:22 -0600

    Assignment 2

---
+++ javascriptBasics/sum.js
@@ -0,0 +1,25 @@
+
+function sum(){
+var num1 = document.getElementById('num1').value;
+var num2 = document.getElementById('num2').value;
+var sum = parseInt(num1) + parseInt(num2);
+alert(sum);
+}
+function subtract(){
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum =  parseInt(num1) - parseInt(num2);
+    alert(sum); 
+}
+function mult(){
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum =  parseInt(num1) * parseInt(num2);
+    alert(sum);
+}
+function divide(){
+    var num1 = document.getElementById('num1').value;
+    var num2 = document.getElementById('num2').value;
+    var sum =  parseInt(num1) / parseInt(num2);
+    alert(sum);
+}
